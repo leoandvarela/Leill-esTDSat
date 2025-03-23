f08124a27e55ffa8013bd71bb2f793edf77304d3
lvv <leonardo.vacari@gmail.com>
2025-03-23 11:11:19 -0300

    Criação do gitignore e melhoria na identação do código

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,8 +4,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -15,19 +13,15 @@ import javax.swing.JOptionPane;
  *
  * @author Adm
  */
-public class conectaDAO {
-    
+public class conectaDAO {    
     public Connection connectDB(){
         Connection conn = null;
         
         try {
-        
             conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
-    }
-    
+    }    
 }
